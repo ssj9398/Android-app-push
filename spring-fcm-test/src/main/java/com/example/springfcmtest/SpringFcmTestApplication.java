ec7765b0a02b5b7db6caf6b6d5ffd92ab95e7f8e
alisyabob <sjs939883@gmail.com>
2022-12-07 01:24:54 +0900

    [CHORE] 스프링 프로젝트 세팅

---
+++ spring-fcm-test/src/main/java/com/example/springfcmtest/SpringFcmTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springfcmtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFcmTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFcmTestApplication.class, args);
+	}
+
+}
---
+++ spring-fcm-test/src/test/java/com/example/springfcmtest/SpringFcmTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springfcmtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFcmTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
